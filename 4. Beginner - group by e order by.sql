92a9358fda30cd5a37c1727ab8f64845bafa7b5d
Leandro S Oliveira <lescherer@gmail.com>
2025-02-14 18:22:44 -0300

    group by e order by

---
+++ 4. Beginner - group by e order by.sql
@@ -0,0 +1,15 @@
+-- Group By
+
+SELECT gender,
+AVG(age),
+MAX(age),
+MIN(age),
+COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
+
+-- ORDER BY
+SELECT *
+FROM employee_demographics
+ORDER BY  first_name DESC
+;
