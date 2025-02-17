d3a2bf8e259789c7954a0f3356f6f4bc30c531b6
Leandro S Oliveira <lescherer@gmail.com>
2025-02-17 08:02:41 -0300

    updade window functions

--- 12. Intermediate - Window Funtions.sql
+++ 12. Intermediate - Window Funtions.sql
@@ -1,2 +1,24 @@
 -- Window Funtions
-4:44
+SELECT dem.first_name,
+dem.last_name,
+AVG(salary) OVER(PARTITION BY gender) __avg_gender
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+SELECT dem.first_name,
+dem.last_name,
+SUM(salary) OVER(PARTITION BY gender ORDER BY dem.employee_id) _Rolling_total
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+SELECT dem.first_name,
+dem.last_name,
+salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num,
+DENSE_RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS dense_rank_num
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;   
