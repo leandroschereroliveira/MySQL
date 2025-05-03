cdc5dbbc40a7f6ecf8c82349d35c9a375b03e865
Leandro S Oliveira <lescherer@gmail.com>
2025-05-03 20:56:12 -0300

    update condicionais

--- Funções Condicionais IF.sql
+++ Funções Condicionais IF.sql
@@ -64,3 +64,78 @@ SET @var2 = 100;
 
 SELECT
 	NULLIF(@var1, @var2);
+    
+    
+# CASE/THEN: A função CASE é uma outra estrutura para tratar condições dentro do SQL.
+# Sintaxe:
+/*
+CASE 
+	WHEN condicao1 THEN resultado1
+    WHEN condicao2 THEN resultado2
+    ELSE resultado
+END;
+*/
+-- Exemplo1: Utilize a estrutura CASE para criar uma consulta da nossa tabelas de clientes que substitui o valor 'M' por 'Masculino' e o valor 'F' por 'Feminino'
+SELECT 
+	*, 
+    CASE
+		WHEN Sexo = 'M' THEN 'Masculino'
+        ELSE 'Feminino'
+    END AS 'Genero'
+    FROM clientes;
+    
+-- Exemplo 2. Na tabela de pedidos, crie uma estrutura CASE para avaliar as seguintes situações de Receita_Venda:
+-- Caso Receita_Venda >= 3000 ---> 'Faturamento Alto'
+-- Caso Receita_Venda >= 1000 ---> 'Faturamento Médio'
+-- Caso Receita_Venda < 1000 ---> 'Faturamento Baixo'
+SELECT 
+	*,
+    CASE 
+		WHEN Receita_Venda >=3000 THEN 'Faturamento Alto'
+        WHEN Receita_Venda >=1000 THEN 'Faturamento Médio'
+        ELSE 'Faturamento Baixo'
+	END AS 'Status'
+    FROM pedidos;
+
+# CASE/THEN: Aplicações com AND e OR
+# Sintaxe (AND):
+/*
+CASE
+	WHEN condicao1 AND condicao2 THEN resultado1
+    ELSE resultado2
+END;
+*/
+# Sintaxe (OR):
+/*
+CASE
+	WHEN condicao1 OR condicao2 THEN resultado1
+    ELSE resultado2
+END;
+*/
+
+-- Aplicação AND
+-- Exemplo1. A empresa está com uma ação de dia das mães/dia dos pais. Como vai funcionar?
+-- Caso o cliente seja do sexo Feminino E tenha filhos, receberá ofertas de dias das mães
+-- Caso o cliente seja do sexo Masculino E tenha filhos, receberá ofertas de dias dos pais
+-- Caso contrário, não receberá oferta
+-- Criar coluna extra identificando cada status.
+SELECT 
+	*,
+    CASE
+		WHEN Sexo = 'F' AND Qtd_Filhos > 0 THEN 'Oferta dia das mães'
+        WHEN Sexo = 'M' AND Qtd_Filhos > 0 THEN 'Oferta dia das mães'
+        ELSE 'Sem oferta'
+    END AS 'Situação'
+    FROM clientes;
+    
+-- APLICAÇÃO OR
+-- Exemplo2. A empresa está com um parceria com as empresas das marcas 'DELL' e 'SAMSUNG'. Isso significa que os produtos dessas marcas receberão um desconto de 15% em seu custo de aquisição.
+-- Faça uma consulta que retorne uma coluna extra de desconto no custo de aquisição de cada produto.
+SELECT
+	*,
+    CASE
+		WHEN Marca_Produto = 'DELL' OR Marca_Produto = 'SAMSUNG' THEN (1 - 0.15) * Custo_Unit
+        ELSE Custo_Unit
+    END AS 'Novo Custo'
+    FROM produtos;
+
