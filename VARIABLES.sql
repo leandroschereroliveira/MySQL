863928ff1fc5547f675a7eae947b400e51a89fb6
Leandro S Oliveira <lescherer@gmail.com>
2025-05-04 11:51:42 -0300

    update VARIABLES

--- VARIABLES.sql
+++ VARIABLES.sql
@@ -30,3 +30,19 @@ SELECT *
 FROM produtos
 WHERE Marca_Produto = @varMarca;
 
+# CAST
+# A função CAST, converte um valor de qualquer tipo em outro tipo de dados especificado.
+-- Obs.: No CAST, especificamos o INT como SIGNED ou UNSIGNED e o VARCHAR como CHAR.
+
+SET @varNumero = 10.9200;
+
+SELECT  @varNumero,
+		CAST(@varNumero AS SIGNED),
+        CAST(@varNumero AS DECIMAL(10,2)),
+        CAST(@varNumero AS CHAR(3));
+        
+SET @varData = '2021-01-01';
+SELECT  @varData,
+		CAST(@varData AS DATE),
+        CAST(@varData AS DATETIME);
+
